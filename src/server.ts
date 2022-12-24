b6e1b0425b42e630d903b637ffe38ae5098b9702
Nathan Seixeiro <nathan.seixeiro@gmail.com>
2022-12-24 16:40:25 -0300

    criando server

---
+++ src/server.ts
@@ -0,0 +1,32 @@
+import './util/module-alias'
+import bodyParser from 'body-parser';
+import { Server } from '@overnightjs/core';
+import { ForecastController } from './controllers/forecast';
+import { Application } from 'express';
+
+export class SetupServer extends Server {
+    constructor(private port = 3000) {
+        super()
+    }
+
+    public init(): void {
+        this.setupExpress()
+        this.setupControllers()
+    }
+
+    private setupExpress(): void {
+        this.app.use(bodyParser.json())
+    }
+
+    private setupControllers() {
+        const forecastController = new ForecastController()
+        this.addControllers([forecastController])
+    }
+
+    /**
+     * getAppp
+     */
+    public getApp(): Application {
+        return this.app
+    }
+}
